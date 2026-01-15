2f89ef6274c14070b6799a16b476018fa0ac9d05
Pranjal Pandey <ppranjal573@gmail.com>
2026-01-16 00:03:55 +0530

    feat : Add new variables in the script.js

--- day-11/script.js
+++ day-11/script.js
@@ -6,3 +6,22 @@ let n = 4;
 let names = [a, b, c, d];
 
 console.log(typeof a, typeof b, typeof c, typeof d, typeof n, typeof names);
+
+let p = null;
+let q = {
+    name: "Atul",
+    age: 24, 
+    isMarried: false,
+    skills: ["JavaScript", "HTML", "CSS"]
+}
+
+console.log(typeof p, typeof q);
+console.log(q);
+
+q.ryan = "Hello Ryan";
+console.log(q);
+delete q.isMarried;
+console.log(q);
+
+q.address = "Nishadganj, Patna";
+console.log(q);

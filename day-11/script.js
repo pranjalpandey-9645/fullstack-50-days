a35a7a97e0661230005a0aaa5f26106394318020
Pranjal Pandey <ppranjal573@gmail.com>
2026-01-15 22:12:35 +0530

    feat: Add the day-11 folder.

---
+++ day-11/script.js
@@ -0,0 +1,8 @@
+let a = "Atul";
+let b = "Bhanu";
+let c = "Chandu";
+let d = "Dinesh";
+let n = 4;
+let names = [a, b, c, d];
+
+console.log(typeof a, typeof b, typeof c, typeof d, typeof n, typeof names);

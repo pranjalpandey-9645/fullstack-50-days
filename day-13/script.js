9abd27e2eae05b44cfdf2cb4c00b17d695fe6b57
pranjalpandey-9645 <ppranjal573@gmail.com>
2026-01-17 23:15:57 +0530

    feat: Add new variable values in script.js

--- day-13/script.js
+++ day-13/script.js
@@ -6,11 +6,33 @@ message = "Hello, World!";
 console.log(message);
 
 let method;
-method = 3+29-50/60;
+method = 3 + 29 - 50 / 60;
 
 console.log(method);
 
-let names;
+let unorganized;
+unorganized = "Hello, World!";
+console.log(unorganized);
+
+let Anime = "Naruto";
+let cruelity = "Attack on Titan";
+let brutality = "Berserk";
+let nobelity = "Code Geass";
 names = [Anime, cruelity, brutality, nobelity];
 
 console.log(names);
+console.log(Anime, typeof names);
+console.log(cruelity, typeof names);
+console.log(brutality, typeof names);
+console.log(nobelity, typeof names);
+
+let age = 18;
+let grace = 18;
+
+if (age < grace) {
+    console.log("You are a minor.");
+} else if (age === grace) {
+    console.log("You are just at the threshold of adulthood.");
+} else {
+    console.log("You are an adult.");
+}

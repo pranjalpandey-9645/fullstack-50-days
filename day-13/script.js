76f1cab10045e6dabd8833c5ee5723b1fa00effc
pranjalpandey-9645 <ppranjal573@gmail.com>
2026-01-17 23:34:09 +0530

    feat: Add opertor with operations in the script.js of day-13 folder.

--- day-13/script.js
+++ day-13/script.js
@@ -29,10 +29,17 @@ console.log(nobelity, typeof names);
 let age = 18;
 let grace = 18;
 
-if (age < grace) {
-    console.log("You are a minor.");
-} else if (age === grace) {
-    console.log("You are just at the threshold of adulthood.");
-} else {
-    console.log("You are an adult.");
-}
+// if (age < grace) {
+//     console.log("You are a minor.");
+// } else if (age === grace) {
+//     console.log("You are just at the threshold of adulthood.");
+// } else {
+//     console.log("You are an adult.");
+// }
+
+console.log(age + grace + " is the sum of age and grace.");
+console.log(age - grace + " is the difference of age and grace.");
+console.log(age * grace + " is the product of age and grace.");
+console.log(age / grace + " is the quotient of age and grace.");
+console.log(age % grace + " is the remainder of age divided by grace.");
+console.log(age ** grace + " is age raised to the power of grace.");
